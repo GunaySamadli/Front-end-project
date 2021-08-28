bc8ba0913611bce628f13c5c4f1be6b7ea51daee
GunaySamadli <gunayfs@code.edu.az>
2021-08-28 10:35:20 +0400

    created footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,3 +17,35 @@ $(document).ready(function () {
     //     $('.lists').slideToggle();
     // })
 });
+
+
+// let ellipsis = document.querySelector("header .icons .ellipsis");
+// let bars = document.querySelector("header .icons .bars");
+
+
+
+// ellipsis.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     let customButton = document.querySelector("header .custom-button");
+//     if (customButton.style.transform == null || customButton.style.transform == "" || customButton.style.transform == "scaleX(0)") {
+//         customButton.style.transform = "scaleX(1)";
+//     } else {
+//         customButton.style.transform = "scaleX(0)";
+//     }
+// });
+
+// bars.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     let customResponsiveNav = document.querySelector("#custom-responsive-nav");
+//     let times = document.querySelector("header .icons .bars .fa-times");
+//     let bars = document.querySelector("header .icons .bars .fa-bars");
+//     if (customResponsiveNav.style.height == null || customResponsiveNav.style.height == "" || customResponsiveNav.style.height == "0px") {
+//         customResponsiveNav.style.height = "333px";
+//         times.style.display = "inline-block";
+//         bars.style.display = "none";
+//     } else {
+//         customResponsiveNav.style.height = "0px";
+//         times.style.display = "none";
+//         bars.style.display = "inline-block";
+//     }
+// });
