bd6c763e9bed5872c38e6b53d5ddb480963d403e
GunaySamadli <gunayfs@code.edu.az>
2021-08-29 18:55:10 +0400

    created responsive navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,58 +1,41 @@
 
+// $(document).ready(function () {
+//     $('.icon-circle-right').on('click', function (e) {
+//         e.preventDefault();
+//         if ($('.drop-menu').css('display') == 'none') {
+//             $('.icon-circle-right').css({
+//                 'transform': 'rotate(90deg)'
+//             });
+//         } else {
+//             $('.icon-circle-right').css({
+//                 'transform': 'rotate(0deg)'
+//             });
+//         }
+//         $('.drop-menu').slideToggle();
+//     })
+// });
 
-$(document).ready(function () {
-    $('.icon-circle-right').on('click', function (e) {
-        e.preventDefault();
-        if ($('.drop-menu').css('display') == 'none') {
-            $('.icon-circle-right').css({
-                'transform': 'rotate(90deg)'
-            });
-        } else {
-            $('.icon-circle-right').css({
-                'transform': 'rotate(0deg)'
-            });
-        }
-        $('.drop-menu').slideToggle();
-    })
 
-    // $('.burger').click(function(){
-    //     $('.lists').slideToggle();
-    // })
+
+
+let icons = document.querySelector("header .openMenu .icon-menu");
+
+
+icons.addEventListener("click", function (e) {
+    e.preventDefault();
+    let customResponsiveNav = document.querySelector(".custom-responsive-nav");
+    let times = document.querySelector("header .openMenu .icon-menu");
+    let bars = document.querySelector("header .openMenu .icon-menu");
+    if (customResponsiveNav.style.height == null || customResponsiveNav.style.height == "" || customResponsiveNav.style.height == "0px") {
+        customResponsiveNav.style.height = "100%";
+        ustomResponsiveNav.style.width = "100%";
+        times.style.display = "inline-block";
+    } else {
+        customResponsiveNav.style.height = "0px";
+        times.style.display = "none";
+        bars.style.display = "inline-block";
+    }
 });
-
-
-
-
-// let ellipsis = document.querySelector("header .icons .ellipsis");
-// let bars = document.querySelector("header .icons .bars");
-
-
-
-// ellipsis.addEventListener("click", function (e) {
-//     e.preventDefault();
-//     let customButton = document.querySelector("header .custom-button");
-//     if (customButton.style.transform == null || customButton.style.transform == "" || customButton.style.transform == "scaleX(0)") {
-//         customButton.style.transform = "scaleX(1)";
-//     } else {
-//         customButton.style.transform = "scaleX(0)";
-//     }
-// });
-
-// bars.addEventListener("click", function (e) {
-//     e.preventDefault();
-//     let customResponsiveNav = document.querySelector("#custom-responsive-nav");
-//     let times = document.querySelector("header .icons .bars .fa-times");
-//     let bars = document.querySelector("header .icons .bars .fa-bars");
-//     if (customResponsiveNav.style.height == null || customResponsiveNav.style.height == "" || customResponsiveNav.style.height == "0px") {
-//         customResponsiveNav.style.height = "333px";
-//         times.style.display = "inline-block";
-//         bars.style.display = "none";
-//     } else {
-//         customResponsiveNav.style.height = "0px";
-//         times.style.display = "none";
-//         bars.style.display = "inline-block";
-//     }
-// });
 
 
 
